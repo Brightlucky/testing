ac9f59a3e323d00cc3401ea444b213ecec3da07b
Brightlucky <51267162+Brightlucky@users.noreply.github.com>
2019-06-02 14:55:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const discord = require("discord.js");
-const token = require("./token.json").token;
-const bot = new discord.Client({
-  disableEveryone: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const clientConfig = require('./internals/tokenfile.json');
+const package = require('./package.json');
+const messages = require('./internals/messages.json');
+const lmao = require('./internals/channel.json');
+client.on('ready', () => {
+  console.log('#-----------------------------------------------------------------------------------------------------------------#')
+  console.log('     ___  _______  ___  __    _          ___      _______  _______  __   __  _______     _______  _______  _______ ')
+  console.log('    |   ||       ||   ||  |  | |        |   |    |       ||   _   ||  | |  ||       |   |  _    ||       ||       |')
+  console.log('    |   ||   _   ||   ||   |_| |        |   |    |    ___||  |_|  ||  |_|  ||    ___|   | |_|   ||   _   ||_     _|')
+  console.log('    |   ||  | |  ||   ||       |  ____  |   |    |   |___ |       ||       ||   |___    |       ||  | |  |  |   |  ')
+  console.log(' ___|   ||  |_|  ||   ||  _    | |____| |   |___ |    ___||       ||       ||    ___|   |  _   | |  |_|  |  |   |  ')
+  console.log('|       ||       ||   || | |   |        |       ||   |___ |   _   | |     | |   |___    | |_|   ||       |  |   |  ')
+  console.log('|_______||_______||___||_|  |__|        |_______||_______||__| |__|  |___|  |_______|   |_______||_______|  |___|  ')
+  console.log('By: Brightluckyface#5487')
+  console.log(`Version: ${package.version}`)
+  console.log(`${client.user.tag} is ready`)
+  console.log('#-----------------------------------------------------------------------------------------------------------------#')
 });
-require("./functions")(bot);
 
-bot.commands = new discord.Collection();
-bot.aliases = new discord.Collection();
-bot.afk = new Map();
 
-module.exports.bot = bot;
+client.on('guildMemberAdd', usr => {
+  let msgchannel_2 = usr.guild.channels.find(channel => channel.id === lmao.channel_id)
+  msgchannel_2.send(messages['joins'].classic.replace('{{member}}', usr.user.tag))
+});
+client.on('guildMemberRemove', usr => {
+  let msgchannel_2 = usr.guild.channels.find(channel => channel.id === lmao.channel_id)
+  msgchannel_2.send(messages['leaves'].classic.replace('{{member}}', usr.user.tag))
+});
 
-bot.login(token);
+client.login(clientConfig.token)
